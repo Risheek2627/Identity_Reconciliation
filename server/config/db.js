858f25c6cd745abf9bfa7c667b03dd3dab14e087
Risheek <risheek2627.com>
2025-06-02 00:15:46 +0530

    small changes in mysql connection

--- server/config/db.js
+++ server/config/db.js
@@ -3,10 +3,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const connection = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "rayaru",
-  database: "bitespeed",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
